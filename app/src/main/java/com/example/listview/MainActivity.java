8634196af10c34d2edaafb54c8f77c66993163a2
amit2703 <amitthakur27march@gmail.com>
2019-06-06 15:23:42 +0530

    ListView

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    ListView list;
+    Spinner list;
     String data[]={"Utkarsh","Amit","Shubham","Raushan","Rajnish"};
 
 
@@ -18,20 +20,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list=(ListView)findViewById(R.id.listview);
+        list=(Spinner) findViewById(R.id.listview);
 
         ArrayAdapter adapter=new ArrayAdapter(MainActivity.this,
                 android.R.layout.simple_list_item_1,data);
         list.setAdapter(adapter);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                TextView txt=(TextView)view;
+               // TextView txt=(TextView)view;
 
-                Toast.makeText(MainActivity.this, txt.getText().toString(),
-                        Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, txt.getText().toString(),
+                   //     Toast.LENGTH_SHORT).show();
+                switch(position)
+                {
+                    case 0:
+                    Toast.makeText(MainActivity.this, "First", Toast.LENGTH_SHORT).show();
+
+                    case 1:
+                        Toast.makeText(MainActivity.this, "Second", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "Third", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(MainActivity.this, "Fourth", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 4:
+                        Toast.makeText(MainActivity.this, "Fifth", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
 
             }
         });
